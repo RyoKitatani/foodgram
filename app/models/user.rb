7ae31088dde397389e5c44450a36c6951da96da2
RyoKitatani <ryo0929m@gmail.com>
2021-03-10 14:10:36 +0000

    userページ実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :foods, dependent: :destroy
   has_many :food_comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  
+  attachment :profile_image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "foods#index"
+  root to: "homes#top"
   resources :foods, only:[:index, :show, :edit, :update, :create, :destroy] do
     resources :food_comments, only:[:create, :destroy]
     resource :likes, only: [:create, :destroy]
   end
-  
+  resources :users, only:[:show, :edit, :update]
 end
