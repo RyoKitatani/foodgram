f4dd8a72cb568a791cc35cf233266666ffb62364
RyoKitatani <ryo0929m@gmail.com>
2021-03-10 22:25:32 +0000

    indexへのいいね機能実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,14 +4,14 @@ class LikesController < ApplicationController
     food = Food.find(params[:food_id])
     like = current_user.likes.new(food_id: food.id)
     like.save
-    redirect_to food_path(food)
+    redirect_to request.referer
   end
   
   def destroy
     food = Food.find(params[:food_id])
     like = current_user.likes.find_by(food_id: food.id)
     like.destroy
-    redirect_to food_path(food)
+    redirect_to request.referer
   end
   
 end
