72572bcf75d2ac49605b35e527ebe30259744a63
RyoKitatani <ryo0929m@gmail.com>
2021-03-11 01:29:12 +0000

    フォロー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
   
   attachment :profile_image
   
+  acts_as_followable
+  acts_as_follower
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,13 @@ Rails.application.routes.draw do
     resource :likes, only: [:create, :destroy]
   end
   resources :users, only:[:show, :edit, :update]
+  
+  put 'users/follow/:user_id' => 'users#follow'
+  put 'users/unfollow/:user_id' => 'users#unfollow'
+  #フォローとフォローを外すアクション
+  
+  get 'users/follow_list/:user_id' => 'users#follow_list'
+  get 'users/follower_list/:user_id' => 'users#follower_list'
+  #フォロー・フォロワーの一覧ページ
+  
 end
